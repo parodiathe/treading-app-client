65cdef78832536e9ba7abefd5d5b9b9f04c9abe2
Daniel M <parodiathe@mail.ru>
2024-08-14 22:46:13 +0300

    routes configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
 import './App.css'
-import Navbar from "@/page/Home/Navbar/Navbar.jsx";
+import Navbar from "@/page/Navbar/Navbar.jsx";
+import Home from "@/page/Home/Home.jsx";
+import {Route, Routes} from "react-router-dom";
+import Portfolio from "@/page/Portfolio/Portfolio.jsx";
+import Wallet from "@/page/Wallet/Wallet.jsx";
+import Activity from "@/page/Activity/Activity.jsx";
+import Withdrawal from "@/page/Withdrawal/Withdrawal.jsx";
+import PaymentDetails from "@/page/Payment Details/PaymentDetails.jsx";
+import StockDetails from "@/page/Stock Details/StockDetails.jsx";
+import Watchlist from "@/page/Watchlist/Watchlist.jsx";
+import Profile from "@/page/Profile/Profile.jsx";
+import SearchCoin from "@/page/Search/SearchCoin.jsx";
+import Notfound from "@/page/Notfound/Notfound.jsx";
 
 function App() {
 
-    return(
+    return (
         <>
-        <Navbar/>
+            <Navbar/>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/portfolio" element={<Portfolio/>}/>
+                <Route path="/activity" element={<Activity/>}/>
+                <Route path="/wallet" element={<Wallet/>}/>
+                <Route path="/withdrawal" element={<Withdrawal/>}/>
+                <Route path="/payment-details" element={<PaymentDetails/>}/>
+                <Route path="/market/:id" element={<StockDetails/>}/>
+                <Route path="/watchlist" element={<Watchlist/>}/>
+                <Route path="/profile" element={<Profile/>}/>
+                <Route path="/search" element={<SearchCoin/>}/>
+                <Route path="*" element={<Notfound/>}/>
+
+
+            </Routes>
         </>
     )
 }
