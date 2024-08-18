66682f97378dbc394f7dab182a436d9902f785f6
Daniel M <parodiathe@mail.ru>
2024-08-18 22:06:50 +0300

    structure slightly changed

--- src/App.jsx
+++ src/App.jsx
@@ -12,27 +12,29 @@ import Watchlist from "@/page/Watchlist/Watchlist.jsx";
 import Profile from "@/page/Profile/Profile.jsx";
 import SearchCoin from "@/page/Search/SearchCoin.jsx";
 import Notfound from "@/page/Notfound/Notfound.jsx";
+import Auth from "@/page/Auth/Auth.jsx";
 
 function App() {
 
     return (
         <>
-            <Navbar/>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/portfolio" element={<Portfolio/>}/>
-                <Route path="/activity" element={<Activity/>}/>
-                <Route path="/wallet" element={<Wallet/>}/>
-                <Route path="/withdrawal" element={<Withdrawal/>}/>
-                <Route path="/payment-details" element={<PaymentDetails/>}/>
-                <Route path="/market/:id" element={<StockDetails/>}/>
-                <Route path="/watchlist" element={<Watchlist/>}/>
-                <Route path="/profile" element={<Profile/>}/>
-                <Route path="/search" element={<SearchCoin/>}/>
-                <Route path="*" element={<Notfound/>}/>
-
-
-            </Routes>
+            <Auth/>
+            {false && <div>
+                <Navbar/>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/portfolio" element={<Portfolio/>}/>
+                    <Route path="/activity" element={<Activity/>}/>
+                    <Route path="/wallet" element={<Wallet/>}/>
+                    <Route path="/withdrawal" element={<Withdrawal/>}/>
+                    <Route path="/payment-details" element={<PaymentDetails/>}/>
+                    <Route path="/market/:id" element={<StockDetails/>}/>
+                    <Route path="/watchlist" element={<Watchlist/>}/>
+                    <Route path="/profile" element={<Profile/>}/>
+                    <Route path="/search" element={<SearchCoin/>}/>
+                    <Route path="*" element={<Notfound/>}/>
+                </Routes>
+            </div>}
         </>
     )
 }
